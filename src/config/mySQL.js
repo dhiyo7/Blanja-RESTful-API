0c549f3907b6dfe51d78be307127d815f7ae8cdd
dhiyo7 <dhiyoreksa12@gmail.com>
2020-12-08 02:41:45 +0700

    23 - F*ck ENV

--- src/config/mySQL.js
+++ src/config/mySQL.js
@@ -1,12 +1,25 @@
 const mySQL = require("mysql");
 
+const { HOST, DB, user, password } = process.env;
+
+
+console.log(HOST);
+console.log(DB);
+console.log(user);
+console.log(password);
+
 // koneksi ke db
 const db = mySQL.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'new_blanja_db',
+    host: HOST,
+    user: user,
+    password: password,
+    database: DB,
     multipleStatements: true
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'dhiyo007',
+    // database: 'new_blanja_db',
+    // multipleStatements: true
 })
 
 // cek koneksi ke db
